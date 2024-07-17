a643a32c656c4657b2ab81d0cf5287cc386cc2c1
He Sichao <1310722434@qq.com>
2024-07-17 16:36:21 +0800

    Implement glucoType algorithm backend

--- algorithm/algorithm/info.py
+++ algorithm/algorithm/info.py
@@ -35,7 +35,8 @@ def predict_gluco(*args, **kwargs):
 @before
 def predict_gluco_type(*args, **kwargs):
     data = request.get_json()
-    json_object = json.loads(data)
+    print(data)
+    # json_object = json.loads(data)
 
     # preprocess
     age = data["age"]
@@ -44,6 +45,8 @@ def predict_gluco_type(*args, **kwargs):
     # 只取glucoList中的predictGluco为一个新的list，glucoList结构为[{'timestamp': [2024, 7, 17, 9, 12, 52], 'predictGluco': 90.0}, {'timestamp': [2024, 7, 17, 9, 12, 33], 'predictGluco': 85.0}]
     cgm_time_series = [gluco["predictGluco"] for gluco in glucoList]
 
+    # print(f'age: {age}, bmi: {bmi}, cgm_time_series: {cgm_time_series}')
     result = glucotype_predict(age, bmi, cgm_time_series)
+    # print(f"result: {result}")
 
     resp(response_body(200, "predict_gluco", result))
